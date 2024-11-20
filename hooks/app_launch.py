2b13a05829620bb686ab5520b5ae152290d35dfc
carlton368 <carlton368@gmail.com>
2024-11-20 17:07:18 +0900

    Update app launch hook to enhance Python path configuration and logging

--- hooks/app_launch.py
+++ hooks/app_launch.py
@@ -133,14 +133,36 @@ class AppLaunch(tank.Hook):
             # else:
                 # on windows, we run the start command in order to avoid
                 # any command shells popping up as part of the application launch.
-            os.environ['PYTHONPATH'] = 'PYTHONPATH_APP_LAUNCH'
-            os.environ['UE_PYTHONPATH'] = 'UE_PYTHONPATH_APP_LAUNCH'
-            os.environ['WONJIN_LAUNCH'] = 'WONJIN_LAUNCH'
+            external_paths = [
+                "external_path3",
+                "external_path4"
+            ]
+            
+            # 새로운 경로들을 세미콜론으로 결합
+            new_paths = os.pathsep.join(external_paths)
+            
+            # PYTHONPATH 설정
+            if 'PYTHONPATH' in os.environ:
+                os.environ['PYTHONPATH'] += os.pathsep + new_paths
+            else:
+                os.environ['PYTHONPATH'] = new_paths
+                
+            # UE_PYTHONPATH 설정
+            if 'UE_PYTHONPATH' in os.environ:
+                os.environ['UE_PYTHONPATH'] += os.pathsep + new_paths
+            else:
+                os.environ['UE_PYTHONPATH'] = new_paths
+            
+            if 'WONJIN_BEFORE_LAUNCH' in os.environ:
+                os.environ['WONJIN_LAUNCH'] += os.pathsep + 'WONJIN_LAUNCH'
+            else:
+                os.environ['WONJIN_LAUNCH'] = 'WONJIN_LAUNCH'
+
             self.parent.log_debug("WONJIN_APP_LAUNCH Updated Python paths:")
             self.parent.log_debug("PYTHONPATH: %s" % os.environ['PYTHONPATH'])
             self.parent.log_debug("UE_PYTHONPATH: %s" % os.environ['UE_PYTHONPATH'])
             self.parent.log_debug("WONJIN_LAUNCH: %s" % os.environ['WONJIN_LAUNCH'])
-            self.parent.log_debug("WONJIN_BEFORE_LAUNCH2: %s" % os.environ['WONJIN_BEFORE_LAUNCH2'])
+            self.parent.log_debug("WONJIN_BEFORE_LAUNCH: %s" % os.environ['WONJIN_BEFORE_LAUNCH'])
             self.parent.log_debug("sys.path: %s" % sys.path)
             cmd = 'start /B "App" "%s" %s' % (app_path, app_args)
             self.parent.log_debug("Command to execute  : %s" % cmd)
